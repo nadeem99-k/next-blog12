a46b0503f398836ffe61a31fbf26bfefaa0fe553
Nadeem Ali <nadeemalikalhoro310@gmail.com>
2024-12-26 14:55:46 +0500

    add  mogoDb and save users from Clerk

--- src/app/lib/mongodb/models/user.model.js
+++ src/app/lib/mongodb/models/user.model.js
@@ -1,48 +1,45 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
 const userSchema = new mongoose.Schema(
-    {
-        clerkId:{
-            type: String,
-            required: true,
-            unique: true,
-        },
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-        firstName: {
-            type: String,
-            required: true,
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-        },
+    profilePicture: {
+      type: String,
+      required: false,
+    },
 
-       lastName: {
-           type: String,
-           required: true
-       },
-
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        
-        profilePicture: {
-            type: String,
-            required: true,
-        
-        },
-
-        isAdmin: {
-            type: Boolean,
-            default: false,
-        }
-}, {timestemps: true}
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
-
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 export default User;
--- src/app/lib/mongodb/mongoose.js
+++ src/app/lib/mongodb/mongoose.js
@@ -1,26 +1,22 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 let initialized = false;
 
-export const connect = async() =>{
-   mongoose.set(`strictQuery`, true);
-   if(initialized){
+export const connect = async () => {
+  mongoose.set('strictQuery', true);
+  if (initialized) {
     console.log('Already connected to MongoDB');
-    
     return;
-   }try{
-        mongoose.connect(process.env.MONGODB_URI, {
-     dbName: 'next-blog', 
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-
-        });
-        console.log('connected to MongoDB');
-
-        initialized = true;
-        
-   }catch (error){
-console.log('Error connecting to MongoDB:', error);
-
-
-}}
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'next-blog',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+    initialized = true;
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error);
+  }
+};
